38d2b2d7c4d52158cf4e5959089e9830e47579bc
Michael644327 <keinsacer@gmail.com>
2024-05-10 16:00:30 +0800

    0510 改標題，加bootstrap icon

--- parts/html-head.php
+++ parts/html-head.php
@@ -3,10 +3,8 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= isset($title) ? "$title | php練習網站" : 'php練習網站' ?></title>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    />
+    <title><?= isset($title) ? "$title | 逃出房間" : '塊陶啊' ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
