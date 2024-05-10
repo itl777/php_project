fd6ad3640cb9a85bb6e154a5eb5a2509980eea73
ez003612 <ez003612@gmail.com>
2024-05-10 18:02:37 +0800

    函式更新

--- users/parts/scripts_map.php
+++ users/parts/scripts_map.php
@@ -1,7 +1,7 @@
 <!-- bootstrap script -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="js/fetchData.js"></script>
+<script src="js/fetch_data.js"></script>
 
 <script src="js/edit_modal_data.js"></script>
 <script src="js/address_modal_data.js"></script>
