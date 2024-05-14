722671241e39761a5921c6feb06864eaf8cbfd7b
ez003612 <ez003612@gmail.com>
2024-05-14 17:14:58 +0800

    更新admin-required.php路徑

--- parts/admin-required.php
+++ parts/admin-required.php
@@ -1,11 +1,12 @@
 <?php
+include __DIR__ . '/../basic-url.php';
 
 if (!isset($_SESSION)) {
   session_start();
 }
 
 if (!isset($_SESSION['admin'])) {
-  header('Location: ../index_.php');
+  header('Location:' .BASE_URL. 'index_.php');
   exit;
 }
 
