dff76304261ea8f266faca360ae10871344dfa6b
tsuncc <tsunmobile010109@gmail.com>
2024-05-15 11:30:11 +0800

    更新 navbar 檔案

--- orders/order-edit.php
+++ orders/order-edit.php
@@ -1,5 +1,5 @@
 <?php include __DIR__ . '/../parts/html-head.php' ?>
-<?php include __DIR__ . '/../parts/navbar.php' ?>
+<?php include __DIR__ . '/../parts/bt-navbar.php' ?>
 
 <div class="container">
   <h2 class="my-5">編輯訂單</h2>
