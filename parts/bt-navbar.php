5fda10f3701b789bcea05946c2caec4b73a17289
IT <sanboul713@gmail.com>
2024-05-15 13:21:58 +0800

    change

--- parts/bt-navbar.php
+++ parts/bt-navbar.php
@@ -32,7 +32,7 @@
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <!-- <div class="sb-sidenav-menu-heading">Core</div> -->
                     <a class="nav-link" href="../users/users.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         會員中心
