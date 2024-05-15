aa2a5c2f3ab32565712cd22ab245f6e6b0175fc8
ez003612 <ez003612@gmail.com>
2024-05-15 09:13:29 +0800

    更新連結

--- users/include/scripts_map.php
+++ users/include/scripts_map.php
@@ -1,8 +1,10 @@
 <!-- bootstrap script -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script> -->
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
 <!-- 工具函式 -->
+
 <script src="js/fetch_data.js"></script>
 <script src="js/modal_toggle.js"></script>
 <!-- <script src="js/clear_form.js"></script> -->
