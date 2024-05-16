7df0d458a8e7542bb8accf23bc216a51d5c27066
Michael644327 <keinsacer@gmail.com>
2024-05-16 10:40:34 +0800

    衝突合併

--- index/index_.php
+++ index/index_.php
@@ -6,7 +6,6 @@ $pageName = 'index';
 
 <?php include __DIR__ . '/../parts/bt-navbar.php' ?>
 
-<<<<<<< HEAD
 <div class="container">
     <div class="row">
         <div class="col-12 text-center pt-4">
@@ -14,11 +13,9 @@ $pageName = 'index';
             <img src="../teams/img/escape.jpeg" width="800" alt="" class="rounded-circle">
         </div>
     </div>
-=======
 <div class="container-fluid home-container">
  <img src="../imgs/ghost.png" alt="">
     
->>>>>>> origin/ez003612
 </div>
 
 
